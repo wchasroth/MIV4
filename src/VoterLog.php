b99bd4c7cb4932572d8d69666c745bf796431b08
Charles Roth <croth@thedance.net>
2026-06-16 15:32:09 -0400

    Start adding voter_log to 2026 interface

---
+++ src/VoterLog.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace CharlesRothDotNet\MIV4;
+
+use CharlesRothDotNet\Alfred\AlfredPDO;
+
+class VoterLog {
+   public static function write(AlfredPDO $alfredPDO, string $message): void {
+
+   }
+
+}
